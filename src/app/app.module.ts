1168fde9d116871ae2fafec3ea36c0c434e91982
Pedro-Dom <pedroaugusto.dom@outlook.com>
2022-05-17 11:26:26 -0300

    Projeto Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { EstudantesComponent } from './estudantes/estudantes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstudantesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-estudantes.ts
@@ -0,0 +1,14 @@
+import { Estudante } from './estudante';
+
+export const ESTUDANTES: Estudante[] = [
+  { id: 1, name: 'Pedrão' },
+  { id: 2, name: 'Marcola' },
+  { id: 3, name: 'Navano' },
+  { id: 4, name: 'Ibazel' },
+  { id: 5, name: 'Gabe' },
+  { id: 6, name: 'Limão' },
+  { id: 7, name: 'Davanzo' },
+  { id: 8, name: 'Nicolino' },
+  { id: 9, name: 'Amate' },
+  { id: 10, name: 'Méc' }
+];
